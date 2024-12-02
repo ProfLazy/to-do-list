bb6add23eb79a6ece99e9b341da9e0fb21f05bce
Haashir Masroor <166060691+ProfLazy@users.noreply.github.com>
2024-12-02 17:46:01 +0000

    Able to add tasks

--- toDoList.py
+++ toDoList.py
@@ -2,7 +2,8 @@ import os
 import time
 
 userInfo = {
-    "Name": "",
+    "taskCounter" : 0,
+    "Name" : "wasd"
 }
 
 def main():
@@ -16,38 +17,27 @@ def main():
     while whileControl != 1:
         userInput = userInputHandler()
 
-        if type(userInput) == str:
-            invalidInput()
-        elif userInput == 1:
+        if userInput == "1":
+            addTask()
+        elif userInput == '2':
             type(userInput)
-        elif userInput == 2:
+        elif userInput == '3':
             type(userInput)
-        elif userInput == 3:
+        elif userInput == '4':
             type(userInput)
-        elif userInput == 4:
+        elif userInput == '5':
             type(userInput)
-        elif userInput == 5:
-            type(userInput)
-        elif userInput == 6:
+        elif userInput == '6':
             whileControl = 1
         else:
             invalidInput()
-
-def invalidInput():
-    print("Please select a valid input")
-    time.sleep(1)
-
-def addTask():
-    
-    
-
 
 def userInputHandler():
 
     # -1 to make sure there is no remnant of the old input
     userInput = -1
 
-    print("1. Add a Tast")
+    print("1. Add a Task")
     print("2. View Tasks")
     print("3. Mark task as completed")
     print("4. Edit a task")
@@ -58,5 +48,18 @@ def userInputHandler():
 
     return userInput
 
+def invalidInput():
+    print("Please select a valid input \n")
+    time.sleep(1)
+
+def addTask():
+    print("Okay, enter your task")
+    userInfo["taskCounter"] += 1
+    userInfo[f"Task {userInfo['taskCounter']}"] = input()
+    print("\n")
+    time.sleep(1)
+
+
 if __name__ == "__main__":
     main()
+    print(userInfo)
